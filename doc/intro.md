e9cd1cec5cea7f041dee41e5b5de2bae59dbda4d
Robert Foy <robert.foy@ventyx.abb.com>
2014-06-11 14:29:29 +1000

    Generator

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to roster-constructor
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/roster_constructor/core_test.clj
@@ -0,0 +1,7 @@
+(ns roster-constructor.core-test
+  (:require [clojure.test :refer :all]
+            [roster-constructor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
